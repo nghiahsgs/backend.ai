6884ec53bf0aa532f0ffaefdccfbe76067c947f0
Kyujin Cho <kyujin.cho@lablup.com>
2024-08-16 14:00:48 +0900

    add network table

--- src/ai/backend/manager/models/__init__.py
+++ src/ai/backend/manager/models/__init__.py
@@ -8,6 +8,7 @@ from . import group as _group
 from . import image as _image
 from . import kernel as _kernel
 from . import keypair as _keypair
+from . import network as _network
 from . import rbac as _rbac
 from . import resource_policy as _rpolicy
 from . import resource_preset as _rpreset
@@ -31,6 +32,7 @@ __all__ = (
     *_image.__all__,
     *_kernel.__all__,
     *_keypair.__all__,
+    *_network.__all__,
     *_user.__all__,
     *_vfolder.__all__,
     *_dotfile.__all__,
@@ -56,6 +58,7 @@ from .group import *  # noqa
 from .image import *  # noqa
 from .kernel import *  # noqa
 from .keypair import *  # noqa
+from .network import *  # noqa
 from .rbac import *  # noqa
 from .resource_policy import *  # noqa
 from .resource_preset import *  # noqa
